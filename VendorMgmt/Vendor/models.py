33ab13722076b4de9d568236e7c55dbff73ccb46
snownisha <snownisha123@gmail.com>
2024-04-29 06:11:39 +0200

    Added model for vendor

--- VendorMgmt/Vendor/models.py
+++ VendorMgmt/Vendor/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class vendor(models.Model):
+    name = models.CharField(max_length=200)
+    contact_details = models.TextField()
+    address = models.TextField()
+    vendor_code = models.CharField(max_length=50)
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()

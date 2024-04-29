8165580c878f09beef612d9fc4405279e40ecbc2
snownisha <snownisha123@gmail.com>
2024-04-29 06:26:33 +0200

    Added model for PurchaseOrder

--- VendorMgmt/PurchaseOrder/models.py
+++ VendorMgmt/PurchaseOrder/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Vendor.models import Vendor
 
 # Create your models here.
+class purchaseOrder(models.Model):
+    po_number = models.CharField(max_length=100, unique=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=50)
+    quality_rating = models.FloatField(null=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField()
--- VendorMgmt/Vendor/models.py
+++ VendorMgmt/Vendor/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class vendor(models.Model):
+class Vendor(models.Model):
     name = models.CharField(max_length=200)
     contact_details = models.TextField()
     address = models.TextField()
-    vendor_code = models.CharField(max_length=50)
+    vendor_code = models.CharField(max_length=50, unique=True)
     on_time_delivery_rate = models.FloatField()
     quality_rating_avg = models.FloatField()
     average_response_time = models.FloatField()

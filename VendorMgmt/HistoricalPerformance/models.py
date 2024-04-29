cdf57237e5d5b030df4d73cb14da0e28d95ba0f1
snownisha <snownisha123@gmail.com>
2024-04-29 06:31:23 +0200

    Added model for HistoricalPerformance

--- VendorMgmt/HistoricalPerformance/models.py
+++ VendorMgmt/HistoricalPerformance/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from Vendor.models import Vendor
 
 # Create your models here.
+class HistoricalPerformance:
+    vendor = models.ForeignKey(Vendor,on_delete=models.CASCADE )
+    date = models.DateTimeField()
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()

d11fe6bd7fb55a97ce65f8d37ddfc4439e835596
snownisha <snownisha123@gmail.com>
2024-04-30 19:48:45 +0200

    created views for Update Acknowledgment Endpoint

--- VendorMgmt/PurchaseOrder/urls.py
+++ VendorMgmt/PurchaseOrder/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import PurchaseOrderListCreate, PurchaseOrderRetrieveUpdateDestroy
+from .views import AcknowledgePurchaseOrder
 
 urlpatterns = [
     path('api/purchase_orders/', PurchaseOrderListCreate.as_view(), name='purchase_order_list_create'),
     path('api/purchase_orders/<int:pk>/', PurchaseOrderRetrieveUpdateDestroy.as_view(), name='purchase_order_retrieve_update_destroy'),
+    path('api/purchase_orders/<int:po_id>/acknowledge/', AcknowledgePurchaseOrder.as_view(), name='acknowledge_purchase_order'),
 ]
--- VendorMgmt/PurchaseOrder/views.py
+++ VendorMgmt/PurchaseOrder/views.py
@@ -1,8 +1,11 @@
+from time import timezone
 from django.shortcuts import render
 from .models import purchaseOrder
 from .serializers import PurchaseOrderSerializer
 from rest_framework import generics
-
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
 class PurchaseOrderListCreate(generics.ListCreateAPIView):
@@ -12,3 +15,19 @@ class PurchaseOrderListCreate(generics.ListCreateAPIView):
 class PurchaseOrderRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = purchaseOrder.objects.all()
     serializer_class = PurchaseOrderSerializer
+
+class AcknowledgePurchaseOrder(APIView):
+    def post(self, request, po_id):
+        try:
+            purchase_order = purchaseOrder.objects.get(pk=po_id)
+        except purchaseOrder.DoesNotExist:
+            return Response({"error": "Purchase Order not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        # Update acknowledgment date
+        purchase_order.acknowledgment_date = timezone.now()
+        purchase_order.save()
+        
+        # Trigger recalculation of average_response_time
+        purchase_order.vendor.calculate_average_response_time()
+        
+        return Response({"message": "Purchase Order acknowledged successfully"}, status=status.HTTP_200_OK)
